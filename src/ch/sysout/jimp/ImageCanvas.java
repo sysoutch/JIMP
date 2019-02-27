088fabf998bf7375c5bbc0c7dd773dcb8466a0c1
RainerWahnsinn <info@sysout.ch>
2019-02-27 14:26:39 +0100

    Initial commit
    Initial commit

---
+++ src/ch/sysout/jimp/ImageCanvas.java
@@ -0,0 +1,441 @@
+package ch.sysout.jimp;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Cursor;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.image.BufferedImage;
+import java.beans.PropertyChangeListener;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.imageio.ImageIO;
+import javax.swing.Action;
+import javax.swing.JPanel;
+import javax.swing.KeyStroke;
+
+import ch.sysout.jimp.ImageCanvas.SelectAllAction;
+
+public class ImageCanvas extends JPanel {
+	private static final long serialVersionUID = 1L;
+
+	private BufferedImage image;
+
+	private int currentTool = -1;
+
+	private Point dragStartPoint;
+	private Point dragEndPoint;
+
+	private float dash1[] = { 10.0f };
+	private BasicStroke dashed = new BasicStroke(1.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 10.0f, dash1, 0.0f);
+
+	private Color defaultSelectColor = Color.LIGHT_GRAY;
+	private Color defaultSelectColor2 = Color.GREEN;
+	private Color backGroundColor = Color.WHITE;
+	private Color currentSelectColor;
+
+	private List<Rectangle> cuttedRects = new ArrayList<>();
+
+	private BufferedImage cuttedImage;
+
+	private BufferedImage pastedImage;
+
+	protected int pressedX;
+	protected int pressedY;
+
+	public static final int SELECT_TOOL = 0;
+	public static final int CROP_TOOL = 1;
+	public static final int ROTATE_TOOL = 2;
+	public static final int COPY_TOOL = 3;
+	public static final int CUT_TOOL = 4;
+	public static final int PASTE_TOOL = 5;
+
+
+	public ImageCanvas() {
+		getInputMap(WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("control A"), "selectAll");
+		getActionMap().put("selectAll", new SelectAllAction());
+		
+		getInputMap(WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("control X"), "cutSelection");
+		getActionMap().put("cutSelection", new CutSelectionAction());
+		
+		getInputMap(WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("control C"), "copySelection");
+		getActionMap().put("copySelection", new CopySelectionAction());
+
+		getInputMap(WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("DELETE"), "deleteSelection");
+		getActionMap().put("deleteSelection", new DeleteSelectionAction());
+		
+		getInputMap(WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("control V"), "pasteSelection");
+		getActionMap().put("pasteSelection", new PasteSelectionAction());
+		
+		addMouseListener(new MouseAdapter() {
+
+			@Override
+			public void mousePressed(MouseEvent e) {
+				super.mousePressed(e);
+				if (getCurrentTool() == SELECT_TOOL) {
+					if (!hasSelection()) {
+						dragStartPoint = e.getPoint();
+						dragEndPoint = null;
+						currentSelectColor = defaultSelectColor;
+					} else {
+						if (getSelectionRectangle().contains(e.getPoint())) {
+							pressedX = e.getX();
+							pressedY = e.getY();
+						} else {
+							dragStartPoint = e.getPoint();
+							dragEndPoint = null;
+							currentSelectColor = defaultSelectColor2;
+						}
+					}
+					repaint();
+				}
+			}
+
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				super.mouseClicked(e);
+				if (hasSelection()) {
+					if (getSelectionRectangle().contains(e.getPoint())) {
+
+					} else {
+						dragStartPoint = null;
+						dragEndPoint = null;
+					}
+				}
+				repaint();
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				super.mouseReleased(e);
+				currentSelectColor = defaultSelectColor2;
+				repaint();
+			}
+		});
+
+		addMouseMotionListener(new MouseMotionAdapter() {
+
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				super.mouseDragged(e);
+				if (getCurrentTool() == SELECT_TOOL) {
+					if (currentSelectColor == defaultSelectColor2) {
+						if (hasSelection()) {
+							Rectangle selectedRectangle = getSelectionRectangle();
+							if (selectedRectangle.contains(e.getPoint())) {
+								int x = e.getX();
+								int y = e.getY();
+								dragStartPoint = new Point(x - (selectedRectangle.width / 2), y - (selectedRectangle.height / 2));
+								dragEndPoint = new Point((x + selectedRectangle.width) - (selectedRectangle.width / 2), (y + selectedRectangle.height) - (selectedRectangle.height / 2));
+							} else {
+							}
+						} else {
+							currentSelectColor = defaultSelectColor;
+							dragEndPoint = e.getPoint();
+						}
+					} else {
+						currentSelectColor = defaultSelectColor;
+						dragEndPoint = e.getPoint();
+					}
+					repaint();
+				}
+			}
+
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				super.mouseMoved(e);
+				if (getCurrentTool() == SELECT_TOOL) {
+					if (hasSelection()) {
+						Cursor cursor = null;
+						if (getSelectionRectangle().contains(e.getPoint())) {
+							cursor = Cursor.getPredefinedCursor(Cursor.MOVE_CURSOR);
+						} else {
+							cursor = Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR);
+						}
+						setCursor(cursor);
+					}
+				}
+			}
+		});
+	}
+
+	public void copySelection() {
+		Rectangle selectedRect = getSelectionRectangle();
+		cuttedImage = image.getSubimage(selectedRect.x, selectedRect.y, selectedRect.width, selectedRect.height);
+	}
+
+	public void cutSelection() {
+		Rectangle selectedRect = getSelectionRectangle();
+		cuttedImage = image.getSubimage(selectedRect.x, selectedRect.y, selectedRect.width, selectedRect.height);
+		cuttedRects.add(selectedRect);
+		dragStartPoint = null;
+		dragEndPoint = null;
+		setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
+		repaint();
+	}
+
+	public void deleteSelection() {
+		Rectangle selectedRect = getSelectionRectangle();
+		cuttedRects.add(selectedRect);
+		dragStartPoint = null;
+		dragEndPoint = null;
+		setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
+		repaint();
+	}
+
+	public void pasteSelection() {
+		pastedImage = cuttedImage;
+		dragStartPoint = new Point(0, 0);
+		dragEndPoint = new Point(cuttedImage.getWidth(), cuttedImage.getHeight());
+		repaint();
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		if (image != null) {
+			Graphics2D g2d = (Graphics2D) g;
+			g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+			g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			g2d.drawImage(image, 0, 0, image.getWidth(null), image.getHeight(null), this);
+			if (cuttedRects.size() > 0) {
+				g2d.setColor(backGroundColor);
+				for (int i = 0; i < cuttedRects.size(); i++) {
+					Rectangle r = cuttedRects.get(i);
+					g2d.
+					fillRect(r.x,
+							r.y, 
+							r.width, 
+							r.height);
+				}
+			}
+
+			if (pastedImage != null) {
+				g2d.drawImage(pastedImage, 0, 0, pastedImage.getWidth(null), pastedImage.getHeight(null), this);
+			}
+
+			if (hasSelection()) {
+				Rectangle rect = getSelectionRectangle();
+				g2d.setColor(currentSelectColor);
+				g2d.setStroke(dashed);
+				g2d.drawRect(rect.x, rect.y, rect.width, rect.height);
+			}
+			g2d.dispose();
+		}
+	}
+
+	public Rectangle getSelectionRectangle() {
+		if (!hasSelection()) {
+			return null;
+		}
+		int startX = (dragStartPoint.x <= dragEndPoint.x) ? dragStartPoint.x : dragEndPoint.x;
+		int startY = (dragStartPoint.y <= dragEndPoint.y) ? dragStartPoint.y : dragEndPoint.y;
+		int width = (dragEndPoint.x >= dragStartPoint.x) ? dragEndPoint.x - dragStartPoint.x
+				: dragStartPoint.x - dragEndPoint.x;
+		int height = (dragEndPoint.y >= dragStartPoint.y) ? dragEndPoint.y - dragStartPoint.y
+				: dragStartPoint.y - dragEndPoint.y;
+		return new Rectangle(startX, startY, width, height);
+	}
+
+	public void setImage(String string) throws IOException {
+		BufferedImage image = ImageIO.read(new FileInputStream(string));
+		this.image = image;
+		setPreferredSize(new Dimension(image.getWidth(null), image.getHeight(null)));
+		repaint();
+	}
+
+	public int getCurrentTool() {
+		return currentTool;
+	}
+
+	public void setCurrentTool(int currentTool) {
+		this.currentTool = currentTool;
+		dragStartPoint = null;
+		dragEndPoint = null;
+		repaint();
+	}
+
+	public boolean hasSelection() {
+		return dragStartPoint != null && dragEndPoint != null;
+	}
+	
+	public class SelectAllAction implements Action {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+		}
+
+		@Override
+		public void addPropertyChangeListener(PropertyChangeListener arg0) {
+		}
+
+		@Override
+		public Object getValue(String arg0) {
+			return null;
+		}
+
+		public boolean isEnabled() {
+			setCurrentTool(SELECT_TOOL);
+			dragStartPoint = new Point(0, 0);
+			dragEndPoint = new Point(image.getWidth(), image.getHeight());
+			repaint();
+			return false;
+		}
+
+		@Override
+		public void putValue(String arg0, Object arg1) {
+		}
+
+		@Override
+		public void removePropertyChangeListener(PropertyChangeListener arg0) {
+		}
+
+		@Override
+		public void setEnabled(boolean arg0) {
+		}
+	}
+	
+	public class CutSelectionAction implements Action {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+		}
+
+		@Override
+		public void addPropertyChangeListener(PropertyChangeListener arg0) {
+		}
+
+		@Override
+		public Object getValue(String arg0) {
+			return null;
+		}
+
+		public boolean isEnabled() {
+			cutSelection();
+			return false;
+		}
+
+		@Override
+		public void putValue(String arg0, Object arg1) {
+		}
+
+		@Override
+		public void removePropertyChangeListener(PropertyChangeListener arg0) {
+		}
+
+		@Override
+		public void setEnabled(boolean arg0) {
+		}
+	}
+	
+	public class CopySelectionAction implements Action {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+		}
+
+		@Override
+		public void addPropertyChangeListener(PropertyChangeListener arg0) {
+		}
+
+		@Override
+		public Object getValue(String arg0) {
+			return null;
+		}
+
+		public boolean isEnabled() {
+			copySelection();
+			return false;
+		}
+
+		@Override
+		public void putValue(String arg0, Object arg1) {
+		}
+
+		@Override
+		public void removePropertyChangeListener(PropertyChangeListener arg0) {
+		}
+
+		@Override
+		public void setEnabled(boolean arg0) {
+		}
+	}
+	
+	public class DeleteSelectionAction implements Action {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+		}
+
+		@Override
+		public void addPropertyChangeListener(PropertyChangeListener arg0) {
+		}
+
+		@Override
+		public Object getValue(String arg0) {
+			return null;
+		}
+
+		public boolean isEnabled() {
+			deleteSelection();
+			return false;
+		}
+
+		@Override
+		public void putValue(String arg0, Object arg1) {
+		}
+
+		@Override
+		public void removePropertyChangeListener(PropertyChangeListener arg0) {
+		}
+
+		@Override
+		public void setEnabled(boolean arg0) {
+		}
+	}
+	
+	public class PasteSelectionAction implements Action {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+		}
+
+		@Override
+		public void addPropertyChangeListener(PropertyChangeListener arg0) {
+		}
+
+		@Override
+		public Object getValue(String arg0) {
+			return null;
+		}
+
+		public boolean isEnabled() {
+			pasteSelection();
+			return false;
+		}
+
+		@Override
+		public void putValue(String arg0, Object arg1) {
+		}
+
+		@Override
+		public void removePropertyChangeListener(PropertyChangeListener arg0) {
+		}
+
+		@Override
+		public void setEnabled(boolean arg0) {
+		}
+	}
+}
---
+++ src/ch/sysout/jimp/MainFrame.java
@@ -0,0 +1,162 @@
+package ch.sysout.jimp;
+
+import java.awt.BorderLayout;
+import java.awt.Cursor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+import javax.imageio.ImageIO;
+import javax.swing.AbstractButton;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JToggleButton;
+import javax.swing.KeyStroke;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.WindowConstants;
+
+import ch.sysout.ui.WrapLayout;
+
+public class MainFrame extends JFrame implements ActionListener {
+	private static final long serialVersionUID = 1L;
+
+	private static final String TITLE = "JIMP - Java Image Manipulation Program";
+
+	private AbstractButton btnSelect = new JToggleButton("Select");
+	private AbstractButton btnCrop = new JToggleButton("Crop");
+	private AbstractButton btnRotate = new JToggleButton("Rotate");
+
+	private AbstractButton btnCopy = new JButton("Copy");
+	private AbstractButton btnCut = new JButton("Cut");
+	private AbstractButton btnPaste = new JButton("Paste");
+	private AbstractButton btnDelete = new JButton("Delete");
+
+	private AbstractButton[] navigationButtons = { btnSelect, btnRotate, btnCrop, btnCopy, btnCut, btnPaste, btnDelete };
+
+	private ImageCanvas pnlImageCanvas = new ImageCanvas();
+
+	private JMenu mnuFile;
+	private JMenu mnuEdit;
+	private JMenu mnuView;
+	private JMenu mnuHelp;
+	private JMenuItem itmNew;
+	private JMenuItem itmOpen;
+	private JMenuItem itmUndo;
+	private JMenuItem itmRedo;
+	private JMenuItem itmHelp;
+
+	public MainFrame() {
+		super(TITLE);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		try {
+			setIconImage(ImageIO.read(new FileInputStream("resources/images/logo.png")));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		initComponents();
+		createAndShowUI();
+	}
+
+	private void initComponents() {
+		ButtonGroup grp = new ButtonGroup();
+		for (AbstractButton btn : navigationButtons) {
+			btn.addActionListener(this);
+			grp.add(btn);
+		}
+	}
+
+	private void createAndShowUI() {
+		JMenuBar mnb = new JMenuBar();
+		mnb.add(mnuFile = new JMenu("File"));
+		mnb.add(mnuEdit = new JMenu("Edit"));
+		mnb.add(mnuView = new JMenu("View"));
+		mnb.add(mnuHelp = new JMenu("Help"));
+		mnuFile.add(itmNew= new JMenuItem("New"));
+		mnuFile.add(itmOpen = new JMenuItem("Open..."));
+		mnuEdit.add(itmUndo = new JMenuItem("Undo"));
+		mnuEdit.add(itmRedo = new JMenuItem("Redo"));
+		mnuHelp.add(itmHelp = new JMenuItem("Help"));
+		
+		itmNew.setAccelerator(KeyStroke.getKeyStroke("control N"));
+		itmOpen.setAccelerator(KeyStroke.getKeyStroke("control O"));
+		itmUndo.setAccelerator(KeyStroke.getKeyStroke("control Z"));
+		itmRedo.setAccelerator(KeyStroke.getKeyStroke("control Y"));
+		itmHelp.setAccelerator(KeyStroke.getKeyStroke("F1"));
+
+		setJMenuBar(mnb);
+		JPanel pnlNavigationBar = new JPanel(new WrapLayout());
+		for (AbstractButton btn : navigationButtons) {
+			pnlNavigationBar.add(btn);
+		}
+		add(pnlNavigationBar, BorderLayout.WEST);
+		JScrollPane sp = new JScrollPane(pnlImageCanvas);
+		sp.getVerticalScrollBar().setUnitIncrement(16);
+		sp.getHorizontalScrollBar().setUnitIncrement(16);
+		add(sp);
+		pack();
+		setSize(1024, 720);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		try {
+			pnlImageCanvas.setImage("resources/images/sample.png");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				try { UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName()); }
+				catch (UnsupportedLookAndFeelException e) {}
+				catch (ClassNotFoundException e) {}
+				catch (InstantiationException e) {}
+				catch (IllegalAccessException e) {}
+				new MainFrame();
+			}
+		});
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object src = e.getSource();
+		if (src == btnSelect) {
+			Cursor cursor = Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR);
+			pnlImageCanvas.setCursor(cursor);
+			pnlImageCanvas.setCurrentTool(ImageCanvas.SELECT_TOOL);
+		} else {
+			if (src == btnCopy) {
+				pnlImageCanvas.copySelection();
+			} else if (src == btnCut) {
+				pnlImageCanvas.cutSelection();
+			} else if (src == btnPaste) {
+				pnlImageCanvas.pasteSelection();
+			} else if (src == btnDelete) {
+				pnlImageCanvas.deleteSelection();
+			} else {
+				pnlImageCanvas.setCursor(null);
+				if (src == btnCrop) {
+					pnlImageCanvas.setCurrentTool(ImageCanvas.CROP_TOOL);
+				}
+				if (src == btnRotate) {
+					pnlImageCanvas.setCurrentTool(ImageCanvas.ROTATE_TOOL);
+				}
+			}
+		}
+	}
+}
